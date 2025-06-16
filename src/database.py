af0cbff0482884952118397375a833f7f95d81a0
Italo Bruno Avelino <52352651+ItaloBrunoM@users.noreply.github.com>
2025-06-16 20:50:15 -0300

    fix(backend): correção do chamado das variaveis

--- src/database.py
+++ src/database.py
@@ -3,7 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://TPPE_USER:TPPE_SENHA@db:5432/TPPE_ESTACIONAMENTO")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+HOST = "db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{HOST}:5432/{DB_NAME}"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
