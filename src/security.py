1a7675da85b23ee6ffc1c51493210caae98f2a9e
Italo Bruno Avelino <52352651+ItaloBrunoM@users.noreply.github.com>
2025-06-21 21:44:47 -0300

    refactor(backend): atualização da biblioteca do hash de senha

--- src/security.py
+++ src/security.py
@@ -1,21 +1,22 @@
+import os
 from datetime import datetime, timedelta, timezone
-from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
+from pwdlib import PasswordHash
 
-SECRET_KEY = "sua-chave-secreta-muito-dificil-de-adivinhar" # Mude isso! Use um segredo forte.
+SECRET_KEY = os.getenv("SECRET_KEY", "uma_chave_de_desenvolvimento_simples_e_longa_sem_problemas")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_hash = PasswordHash.recommended()
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verifica se a senha em texto plano corresponde ao hash."""
-    return pwd_context.verify(plain_password, hashed_password)
+    return password_hash.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
     """Gera o hash de uma senha."""
-    return pwd_context.hash(password)
+    return password_hash.hash(password)
 
 def create_access_token(data: dict):
     """Cria um novo token de acesso (JWT)."""
